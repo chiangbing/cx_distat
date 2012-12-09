fa434f5ff7a0871dd8959f854ce7e80b874c60ff
Clay Chiang <chiangbing@gmail.com>
2012-12-10 02:52:32 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+DistStat
+========
+
+A simple utility for collect CPU/MEM/DISK/NET statistics from small cluster.
+
+<b>distat</b> is designed as a lightweight and standalone tool to collects statistics 
+from cluster. Current statistics include:
+<ul>
+    <li>CPU usage</li>
+    <li>Memory usage</li>
+    <li>Disk read rate (in KB)</li>
+    <li>Disk write rate (in KB)</li>
+    <li>Network read rate (in KB)</li>
+    <li>Network write rate (in KB)</li>
+<ul> 
+
+Usage
+=====
+<ol>
+    <li>Edit configuration distat-env.sh (optional, maybe default setting is cool for you).</li>
+    <li>Run distat.</li>
+</ol>
---
+++ distat-env.sh
@@ -0,0 +1,45 @@
+# Max size of result file
+# Default: 1M
+# MAX_FILESIZE=1048576
+
+# Max open time of result file
+# Default: 10 years
+# MAX_FILETIME=3153600000
+
+# Max sequence number of result file (as suffix)
+# Default to 9999
+# MAX_SEQ=9999
+
+# Temporary directory for intermediate files.
+# TMP_DIR=tmp
+
+# PID_FILE contains pid of running script (for manually killing)
+# PID_FILE=
+
+# Log directory
+# LOG_DIR=logs
+
+# Result file directory
+# RESULT_DIR=results
+
+# A list of hosts to collect statistics
+# Default: localhost
+# SLAVES=(
+# localhost
+# )
+
+# Net interface device name pattern
+# Default: bond0
+# NET_IFACE=bond0
+
+# Disks device name prefix
+# Default: sd
+# DISK_PREFIX
+
+# Result file field delimiter
+# Default: ,(comma)
+# DELIMITER
+
+# Collecting interval
+# Default: 1 minute
+# INTERVAL=5
